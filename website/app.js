8d93a03c809e04ab629a7595297888b7d243b3d4
MarcoNashaat <75084752+MarcoNashaat@users.noreply.github.com>
2021-06-08 11:39:04 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,73 +1,54 @@
 /* Global Variables */
-const baseURL=`https://api.openweathermap.org/data/2.5/weather?zip=`;
-const apikey=`&appid=f4d693f0f3b21ac778a9e47c00bb241f`;
+const key='f4d693f0f3b21ac778a9e47c00bb241f'
 
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-//event on generate button
-document.getElementById('generate').addEventListener('click',appFunction());
+//getting informatin from user
+zip=document.getElementById('zip');
 
-//declaring apFunction
-function appFunction(){
-    //getting zip code user entered
-    let zip=document.getElementById('zip').value;
 
-    //getting feelings user entered
-    let feelings=document.getElementById('feelings').value;
+//async function to fetch data
+document.getElementById('generate').addEventListener('click',async()=>{
+    const res=await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip.value}&appid=${key}&units=metric`);
+    const data=await res.json();
+    let temp=data.main.temp;
 
-    //async function to get weather data
-    const weatherData= async(baseURL,zip,apikey)=>{
-        //fetching data based on user entered zip code
-        const response=await fetch(baseURL+zip+apikey);
+     //console.log(data);  //testing
 
-        try {
-            //turning data into json data
-            const data=await response.json();
-            console.log(data);
-           
-        
-           
-            //catching errors
-        } catch (error) {
-            console.log('error found'+error);
-        }    
-    }
-
-    //async function to post data on server
-    const postData=async(url='',projectData={})=>{
-        const response=await fetch(url,{
+// posting data to server
+const postData=await fetch('/postRoute',{
             method:'POST',
             credentials:'same-origin',
-            body:json.stringfy(data),
             headers:{
                 'Content-Type':'application/json'
-            }
-        })
-
-        try {
-            const recievedData=await response.json();
-            console.log(recievedData);
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    //async function for interface updating 
-    const interfaceUpdate= async()=>{
-        const request=await fetch ('/getRoute')
-
-        try {
-            const data= await request.json();
-            document.getElementById('date').innerHTML="Date: "+d;
-            document.getElementById('temp').innerHTML="Temperature: "+data[0].temp;
-            document.getElementById('content').innerHTML="I feel "+data[0].content;
-        } catch (error) {
-            console.log(error)
+            },
+            body: JSON.stringify({
+                date:newDate,
+                temp:temp,
+                feelings:document.getElementById('feelings').value
+            })
             
-        }
-    }
-}
+});
+const returnedData=await fetch('/getRoute',{
+    'credentials':'same-origin'
+})
+
+// const y=returnedData.json();  //testing
+// console.log(y)
+
+
+
+//updating interface
+
+     const finalData= await returnedData.json();
+     //console.log(finalData);  //testing
+    document.getElementById('date').innerHTML="Date: "+finalData.date;
+    document.getElementById('temp').innerHTML="Temp: "+finalData.temp;
+    document.getElementById('content').innerHTML="I feel "+finalData.feelings;
+
+
+})
 
