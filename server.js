ea66879dd3ab5a8f33847b282ff81ac49131514d
MarcoNashaat <75084752+MarcoNashaat@users.noreply.github.com>
2021-06-08 11:38:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,12 @@ projectData = {};
 
 // Require Express to run server and routes
 const express=require('express');
-
-//Require cors and body parser
 const cors=require('cors');
 const bodyParser=require('body-parser');
+const { request, response } = require('express');
 
 // Start up an instance of app
 const app=express();
-
-//Setting port to listen to
-const port=3200;
-
-//server testing
-server=app.listen(port,function(){
-    console.log(`server running on port ${port}`)
-})
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -32,27 +23,23 @@ app.use(express.static('website'));
 
 
 // Setup Server
-//setting up a get route
-app.get('/getRoute',getCallback())
-//get function
-function getCallback(req,res){
-   
-    res.send(projectData);
-    
-    console.log(projectData);
-}
+const port=4500;
+app.listen(port,()=>{
+    console.log('server running on port '+port)
+})
 
-//setting up a post route
-app.post('/postRoute',postCallback())
-//post function
+//get route
+app.get('/getRoute',(request,response)=>{
+    response.send(projectData);
+})
 
-function postCallback(request,response){
-    let data=request.body;
-    newData={
-        temperature:data.temperature,
-        date:data.date,
-        content:data.content
-    }
+//post route
+app.post('/postRoute',(request,response)=>{
+    console.log(request.body);
+    projectData.temp=request.body.temp;
+    projectData.date=request.body.date;
+    projectData.feelings=request.body.feelings;
 
-    projectData.push(newData);
-}
+    response.end();
+})
+
